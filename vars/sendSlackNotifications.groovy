ecd357719db20b6892d240de3dafeefd34adc74f
dohaugustin <122422951+dohaugustin@users.noreply.github.com>
2024-05-15 20:36:27 -0400

    Update sendSlackNotifications.groovy

--- vars/sendSlackNotifications.groovy
+++ vars/sendSlackNotifications.groovy
@@ -23,5 +23,5 @@ def call(String buildStatus = 'STARTED') {
   }
 
   // Calling the slackSend function to Send notifications.
-  slackSend (color: colorCode, message: summary, channel: '#test-project')
+  slackSend (color: colorCode, message: summary, channel: '#glory_app')
 }
